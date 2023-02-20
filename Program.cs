7817c67510d467640cdb896b6c4ac9e5b0b0cc81
Jordane HIRT <HIRT.Jordane@rotarex.com>
2023-02-20 17:03:07 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Configuration;
+
+Console.WriteLine("Hello, World!");
+
+var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+var endpoint = config["CosmosEndpoint"];
+var masterKey = config["CosmosMasterKey"];
+
+using (var client = new CosmosClient(endpoint, masterKey))
+{
+    var container = client.GetContainer("Families", "Families");
+    var sql = "SELECT * FROM c WHERE ARRAY_LENGTH(c.children) > 1";
+    var iterator = container.GetItemQueryIterator<dynamic>(sql);
+    var page = await iterator.ReadNextAsync();
+
+    foreach (var doc in page)
+    {
+        Console.WriteLine($"Family {doc.id} has {doc.children.Count} children");
+    }
+
+    Console.ReadLine();
+}
